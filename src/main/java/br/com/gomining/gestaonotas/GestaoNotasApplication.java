e38521f6283bd690f64a0765306bd342d75156ff
Cassio Ribeiro <cassioribeiropereira@gmail.com>
2021-01-31 12:52:36 -0300

    feat(gestao-pessoas): criar projeto.

---
+++ src/main/java/br/com/gomining/gestaonotas/GestaoNotasApplication.java
@@ -0,0 +1,13 @@
+package br.com.gomining.gestaonotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoNotasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoNotasApplication.class, args);
+    }
+
+}
